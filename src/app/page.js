cfcf02511f0235d3d7812bb7e26cd292e1345262
Saurabh Mhatre <codeclassifiers@gmail.com>
2023-08-13 20:16:37 +0530

    First commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,125 @@
-import Image from 'next/image'
+'use client';
+import { useRouter } from 'next/navigation';
+import {
+  Client,
+  Provider,
+  cacheExchange,
+  fetchExchange,
+  gql,
+  useQuery,
+} from 'urql';
+import Header from './components/Header';
+const client = new Client({
+  url: 'https://grafbase-test-saurabhnative.grafbase.app/graphql',
+  exchanges: [cacheExchange, fetchExchange],
+});
 
-export default function Home() {
+const GetBlogs = gql`
+  query Contentful {
+    contentful {
+      subatomicaBlogPostCollection {
+        total
+        items {
+          blogTitle
+          blogDescription
+          blogImage {
+            url
+          }
+          postnumber
+        }
+      }
+    }
+  }
+`;
+
+function Home() {
+  const router = useRouter();
+  const [result, reexecuteQuery] = useQuery({
+    query: GetBlogs,
+  });
+  const { data, fetching, error } = result;
+  if (fetching) {
+    return <div>Loading...</div>;
+  }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <Header />
+      <section>
+        <div class="bg-gray-100 sm:grid grid-rows-1 px-4 py-6 space-y-6 sm:space-y-0 sm:gap-4">
+          <div class="h-auto lg:h-96 py-10 col-span-4 bg-gradient-to-tr from-indigo-800 to-indigo-500 rounded-md flex items-center">
+            <div class="ml-20 w-100">
+              <h2 class="text-white text-3xl">What is Subatomica?</h2>
+              <p class="text-indigo-100 mt-4 capitalize tracking-wider leading-7 font-normal pr-8">
+                Most of us have heard about protons, electrons, and neutrons.
+                Have you ever wondered what these particles are themselves made
+                up of? <br></br>Welcome to Subatomica: Unveiling the Hidden
+                Universe of Elementary Particles! Step into a world where the
+                tiniest building blocks hold the key to understanding the
+                grandest mysteries of the cosmos. Subatomica is your portal to
+                the captivating realm of subatomic particles, where electrons,
+                quarks, neutrinos, and their elusive cousins come to life
+                through captivating narratives and insightful exploration.
+              </p>
+
+              <a
+                href="#blogarticles"
+                class="uppercase inline-block mt-8 text-sm bg-white py-2 px-4 rounded font-semibold hover:bg-indigo-100"
+              >
+                get started
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div className="pb-12 bg-gray-100" id="blogarticles">
+        <div class="container px-5 mx-auto">
+          <div class="flex flex-wrap -m-4">
+            {data &&
+              data.contentful.subatomicaBlogPostCollection.items.map((item) => {
+                return (
+                  <div class="p-4 md:w-1/3">
+                    <div
+                      onClick={() => router.push(`/blog/${item.postnumber}`)}
+                      className="cursor-pointer"
+                    >
+                      <div class="h-full rounded-xl shadow-cla-blue bg-gradient-to-r from-indigo-50 to-blue-50 overflow-hidden">
+                        <img
+                          class="lg:h-48 md:h-36 w-full object-contain object-center scale-110 transition-all duration-400 hover:scale-100"
+                          src={item.blogImage.url}
+                          alt="blog"
+                        />
+                        <div class="p-6">
+                          <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
+                            Elementary particle
+                          </h2>
+                          <h1 class="title-font text-lg font-medium text-gray-600 mb-3">
+                            {item.blogTitle}
+                          </h1>
+                          <p class="leading-relaxed mb-3 overflow-hidden blog-description">
+                            {item.blogDescription}
+                          </p>
+                          <div class="flex items-center flex-wrap ">
+                            <button class="bg-gradient-to-r from-cyan-400 to-blue-400 hover:scale-105 drop-shadow-md  shadow-cla-blue px-4 py-1 rounded-lg">
+                              Learn more
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
+
+const App = () => (
+  <Provider value={client}>
+    <Home />
+  </Provider>
+);
+
+export default App;
